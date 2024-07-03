6872cd94d0a3dda26fc977e333ab53c354791e3c
Vitaliy Ushakov <vitaliy-ushak0v@ya.ru>
2024-07-03 23:03:43 +0300

    Практическое задание 2. Реализовал страницу Hello, world.

---
+++ tgBot/src/test/java/ru/vitaliy/tgBot/TgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vitaliy.tgBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
