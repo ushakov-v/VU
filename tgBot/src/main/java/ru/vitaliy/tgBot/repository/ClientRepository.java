4915b791aab2267b56888f1e2e26b26a4926d903
Vitaliy Ushakov <vitaliy-ushak0v@ya.ru>
2024-07-06 02:26:11 +0300

    Исправлен ClientRepository

--- tgBot/src/main/java/ru/vitaliy/tgBot/repository/ClientRepository.java
+++ tgBot/src/main/java/ru/vitaliy/tgBot/repository/ClientRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.vitaliy.tgBot.entity.Client;
 
-@RepositoryRestResource(collectionResourceRel = "clients", path = "clientss")
+@RepositoryRestResource(collectionResourceRel = "clients", path = "clients")
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
 }
