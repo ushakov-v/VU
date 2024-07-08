c2869d39662e2e1cefd89bc8512852633b2339b7
Vitaliy Ushakov <vitaliy-ushak0v@ya.ru>
2024-07-09 02:50:39 +0300

    Выполнено практическое задание №4

---
+++ tgBot/src/main/java/ru/vitaliy/tgBot/controller/ApplicationRestController.java
@@ -0,0 +1,39 @@
+package ru.vitaliy.tgBot.controller;
+
+import org.springframework.web.bind.annotation.*;
+import ru.vitaliy.tgBot.entity.ClientOrder;
+import ru.vitaliy.tgBot.entity.Product;
+import ru.vitaliy.tgBot.service.AppService;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/rest")
+public class ApplicationRestController {
+    private final AppService appService;
+
+    public ApplicationRestController(AppService appService) {
+        this.appService = appService;
+    }
+
+    @GetMapping("/products/search")
+    public List<Product> getProductsByCategoryId(@RequestParam Long categoryId) {
+        return appService.getProductsByCategoryId(categoryId);
+    }
+
+    @GetMapping("/clients/{id}/orders")
+    public List<ClientOrder> getClientOrders(@RequestParam Long id) {
+        return appService.getClientOrders(id);
+    }
+
+    @GetMapping("/clients/{id}/products")
+    public Set<Product> getAllProductsInClientId(@PathVariable Long id){
+        return appService.getAllProductsInClientId(id);
+    }
+
+    @GetMapping("/products/popular")
+    public List<Product> getPopularProducts(@RequestParam Integer limit) {
+        return appService.getPopularProducts(limit);
+    }
+}
--- tgBot/src/main/java/ru/vitaliy/tgBot/repository/ClientOrderRepository.java
+++ tgBot/src/main/java/ru/vitaliy/tgBot/repository/ClientOrderRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.vitaliy.tgBot.entity.ClientOrder;
 
+import java.util.List;
+
 @RepositoryRestResource(collectionResourceRel = "clientOrders", path = "clientOrders")
 public interface ClientOrderRepository extends JpaRepository<ClientOrder, Long> {
-
+    List<ClientOrder> findByClientId(Long id);
 }
